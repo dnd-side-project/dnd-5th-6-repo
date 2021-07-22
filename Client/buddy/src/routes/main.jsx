dba1f9195acddde37e2f2c7a467a694f3f4c330a
JBG0000 <practice9090@naver.com>
2021-07-22 10:03:45 +0900

    [feat] KaKao login 2

--- Client/buddy/src/routes/main.jsx
+++ Client/buddy/src/routes/main.jsx
@@ -3,6 +3,7 @@ import SelectOption from "../components/selectOption/selectOption";
 import NavBar from "../components/navBar/navBar";
 import NaverLogin from "../components/login/naverLogin";
 import CardList from "./../components/cardList/cardList";
+import CardItem from "../components/cardItem/cardItem";
 
 function Main() {
   return (
