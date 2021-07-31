bb0d18f508400329e4aa99fce513ddc2bba13b2b
Song-YunMin <thd930308@naver.com>
2021-07-31 23:58:31 +0900

    [refactor] 좋아요 누르기 기능 변경

--- Server/lib/resolver-utils/likePost.js
+++ Server/lib/resolver-utils/likePost.js
@@ -2,22 +2,28 @@ const {jwtDecode} = require('../resolver-utils/UserAuth');
 
 const updatePostByLike = async (token, args, context) => {
     let userIndex = -1;
-    const {postIndex} = args;
+    const {postIndex, isDuplicate} = args;
     const decode = jwtDecode(token.split(' ')[1])
     if (decode === null) {
         throw new Error('Invalid_Token')
     } else {
         userIndex = decode.ID;
     }
-    try {
+
+    if (isDuplicate === true) {
+        await context.prisma.like.delete({
+            where: {
+                postIndex: postIndex
+            }
+        })
+    } else {
         await context.prisma.like.create({
             data: {
                 userIndex: userIndex,
                 postIndex: postIndex
             }
         })
-    } catch(err) {
-        throw new Error('좋아요 누르기 실패!');
+
     }
     return true;
 }
