02c2f31e651f1a1da2ab3f650ac081125591c949
JBG0000 <practice9090@naver.com>
2021-08-07 13:31:34 +0900

    [feat]MainFeed css 정리

--- Client/buddy/src/components/navBar/navBar.jsx
+++ Client/buddy/src/components/navBar/navBar.jsx
@@ -4,59 +4,25 @@ import { NavItem, NavLink } from "reactstrap";
 import { Hamburger, PaceBuddy } from "../../icons";
 import { Link } from "react-router-dom";
 
-// function NavBar() {
+function NavBar() {
 
-const Navbar = () => {
-  const [isOpen, setNav] = useState(false); //useState의 기능은 this.state와 유사하다, 초기값을 false로 설정
-  const toggleNav = () => {
-    setNav((isOpen) => !isOpen); //on, off 개념의 boolean
-  };
+// const Navbar = () => {
 
-  return (
-    <div>
-      {/* 위 작성 함수 호출 */}
-      {/* pacebuddylogo */}
-      <div>
+  return (<>
+      <div className={styles.height}>
         <Link to="/">
           <PaceBuddy />
         </Link>
-        {/* <button className={styles.hambtn} onClick={toggleNav}>햄버거바</button>  */}
-        <Hamburger />
-        {/* <svg
-          className={styles.hambtn}
-          onClick={toggleNav}
-          width="28"
-          height="28"
-          viewBox="0 0 28 28"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M5.59998 7H22.4M11.9724 14H22.4M7.91722 21H22.4"
-            stroke="#474747"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg> */}
+
+        {/* navbar 페이지 분기로 수정, 라우팅 시 애니메이션 효과를 따로 적용 */}
+        <Link className={styles.hambtn} to="/navbar">
+          <Hamburger />
+        </Link>
       </div>
-      <div className={isOpen ? `${styles.showmenu}` : `${styles.hidemenu}`}>
-        {/* `${styles.navLinks} ${styles.showNav}` : `${styles.navLinks}`} */}
-        <div className={styles.list}>
-          <NavItem className={styles.listitem}>
-            <NavLink href="/">Feed</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/record/">record</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/report">report</NavLink>
-          </NavItem>
-        </div>
-      </div>
-    </div>
+      <hr className={styles.line}></hr>
+      </>
   );
 };
 // }
 
-export default Navbar;
+export default NavBar;
