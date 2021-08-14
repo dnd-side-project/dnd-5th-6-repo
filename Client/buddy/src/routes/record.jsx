6080040c915b012ff79483898309639dfa93d0ac
Song-YunMin <thd930308@naver.com>
2021-08-15 02:47:46 +0900

    [docs] 배포용 수정

--- Client/buddy/src/routes/record.jsx
+++ Client/buddy/src/routes/record.jsx
@@ -2,12 +2,12 @@ import Calendar from "components/calendar/calendar";
 import React, { useEffect } from "react";
 import TwoButtonModal from "../components/modal/twoButtonModal";
 import NavBar from "./../components/navBar/navBar";
-import { EmptyCard, RecordBtn } from "./../icons";
+import { EmptyCard, RecordBtn } from "../icons";
 import { Link } from "react-router-dom";
 import { Navbar } from "components/navBar/navBar";
 import RecordBar from "./../components/recordBar/recordBar";
 import { useQuery } from "@apollo/client";
-import { GET_MY_CARD } from "./../apollo/queries/cardItem/getCard";
+import { GET_MY_CARD } from "../apollo/queries/cardItem/getCard";
 import CardList from "./../components/cardList/cardList";
 
 function Record() {
