a478908b6f2dc5b30b77c8ad41843380d1d006f3
jihyun12 <kkjjhhbb@naver.com>
2021-08-16 20:33:18 +0900

    [hotfix] 파일명 변경

--- Client/buddy/src/apollo/queries/CardItem/like.js
+++
@@ -1,7 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const TOGGLE_LIKE = gql`
-  mutation AddLike($postIndex: Int!, $isDuplicate: Boolean!) {
-    likePost(postIndex: $postIndex, isDuplicate: $isDuplicate)
-  }
-`;
