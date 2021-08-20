5f75190b168d01ddc3c4dda06f5c62c4745c8f14
JBG0000 <practice9090@naver.com>
2021-08-20 19:15:48 +0900

    [style] 로그인 및 모달창 css 수정

--- Client/buddy/src/components/login/loginContents.jsx
+++ Client/buddy/src/components/login/loginContents.jsx
@@ -26,7 +26,7 @@ function LoginContents() {
       <div>
         <p className={styles.write}>반가워요!</p>
         <p className={styles.write}>버디와 함께</p>
-        <p className={styles.write}>운동을 기록해 볼까요?</p>
+        <p className={styles.write}>운동을 기록해볼까요?</p>
       </div>
       <div className={styles.Logincharter}>
         <LoginCharter></LoginCharter>
